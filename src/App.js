2309d08f85fd5b52e916446b2b100ce5c622fc33
imonoho <imonoho@gmail.com>
2020-09-14 20:14:47 -0500

    Some bottons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Buttons from "./Buttons";
 import './App.css';
+import ButtonsWithNumbersInArray from "./ButtonsWithNumbersInArray";
 
 function App() {
   return (
     <div className="App">
       <Buttons/>
+      <ButtonsWithNumbersInArray/>
     </div>
   );
 }
---
+++ src/ButtonsWithNumbersInArray.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function ButtonsWithNumbersInArray() {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ButtonsWithNumbersInArray;
